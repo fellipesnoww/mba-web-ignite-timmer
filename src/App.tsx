3f646c8f252772de2f24fdf964a06fcdbaf57d48
fellipesnoww <fellipe2.0e@gmail.com>
2025-04-29 20:56:55 -0300

    config: eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/themes/global";
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/themes/global';
 
 export function App() {
 
@@ -12,5 +12,5 @@ export function App() {
         </h1>
       </GlobalStyle>
     </ThemeProvider>
-  )
+  );
 }
