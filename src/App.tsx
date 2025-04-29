25d4447d56eebe3791f3db18596644cc30bd50f7
fellipesnoww <fellipe2.0e@gmail.com>
2025-04-29 19:54:59 -0300

    chore: config and theme with styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { ThemeProvider } from "styled-components";
+import { defaulTheme } from "./styles/theme/default";
+import { GlobalStyle } from "./styles/theme/global";
 
 export function App() {
 
   return (
-    <h1>
-      Hello
-    </h1>
+    <ThemeProvider theme={defaulTheme}>
+      <GlobalStyle>
+        <h1>
+          Hello
+        </h1>
+      </GlobalStyle>
+    </ThemeProvider>
   )
 }
