271b6ecbf17894c884c04801177202ec0441adfe
fellipesnoww <fellipe2.0e@gmail.com>
2025-04-29 20:12:59 -0300

    fix: theme config

--- src/@types/styles.d.ts
+++ src/@types/styles.d.ts
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
 import 'styled-components';
 import { defaultTheme } from '../styles/themes/default';
 
 type ThemeType = typeof defaultTheme;
 
 declare module 'styled-components' {
-    export interface DefaultTheme extends ThemeType {}
+  export interface DefaultTheme extends ThemeType {}
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { ThemeProvider } from "styled-components";
-import { defaulTheme } from "./styles/theme/default";
-import { GlobalStyle } from "./styles/theme/global";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/themes/global";
 
 export function App() {
 
   return (
-    <ThemeProvider theme={defaulTheme}>
+    <ThemeProvider theme={defaultTheme}>
       <GlobalStyle>
         <h1>
           Hello
